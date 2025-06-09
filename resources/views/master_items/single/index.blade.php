6a11b5c49aee3e9bcd9f8e365d8542a598939e53
MyukaQA <mylian214@gmail.com>
2025-06-09 15:36:02 +0700

    update list

--- resources/views/master_items/single/index.blade.php
+++ resources/views/master_items/single/index.blade.php
@@ -49,6 +49,17 @@
                             <td>:</td>
                             <td>{{$data->jenis}}</td>
                         </tr>
+                        <tr>
+                            <th>Kategori</th>
+                            <td>:</td>
+                            <td>
+                                 <ol>
+                                    @foreach ($data->categoryitems()->get() as $item)
+                                        <li>Nama Kategori : {{ $item->nama }}</li>
+                                    @endforeach
+                                 </ol>
+                            </td>
+                        </tr>
                     </table>
                     <a class="btn btn-info" href="{{url('master-items/form/edit')}}/{{$data->id}}">Edit</a>
                     <a class="btn btn-danger" href="{{url('master-items/delete')}}/{{$data->id}}" onclick="return confirm('Are you sure you want to delete this item?');">Delete</a>
